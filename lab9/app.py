c0ea1715eddd7a88c10edd2b5bbbd7878952e12d
Kushan Shah <kushanshah1901@gmail.com>
2021-10-22 02:55:35 +0530

    Lab 9 completed

---
+++ lab9/app.py
@@ -0,0 +1,38 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, abort
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+        name = request.form.get('name')
+        if not name:
+            return abort(404, description = 'Name missing')
+        day = request.form.get('day')
+        if not day:
+            return abort(404, description = 'Day missing')
+        month = request.form.get('month')
+        if not month:
+            return abort(404, description = 'Month missing')
+        birthdays = db.execute('INSERT INTO birthdays(name, month, day) values (?, ?, ?)', name, month, day)
+        return redirect("/")
+
+    else:
+
+        # TODO: Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays = birthdays)
+
+
